cf7a1d8a29d6680e999e3ffad8df66db6fa6b3d7
Jocelyn Villicana <villi.jvv@gmail.com>
2019-04-24 18:40:24 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -8,8 +8,8 @@ def display_board (board)
 
 end
 
-def valid_move? (board, index)
-    index.between? (0, 8) && !position_taken? (board, index)
+def valid_move?(board, index)
+    index.between?(0, 8) && !position_taken?(board, index)
 end
 
 def position_taken?(board, index)
