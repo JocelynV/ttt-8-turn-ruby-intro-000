606d419bdd2699bb9eaf6e03a174181d17201d19
Jocelyn Villicana <villi.jvv@gmail.com>
2019-04-24 18:00:44 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -10,5 +10,14 @@ end
 
 def valid_move? (board, index)
     index.between? (0,8) && !position_taken? (board, index)
+end
+
+def position_taken(board, index)
+  if board index == "" || board index == " " || board index == nil
+    return false
+  else 
+    return true
+    
+  end
 
 end
