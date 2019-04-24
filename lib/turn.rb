b97fddd492f87d30515742ce6cd22024a89ab1c9
Jocelyn Villicana <villi.jvv@gmail.com>
2019-04-24 18:36:34 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -9,11 +9,11 @@ def display_board (board)
 end
 
 def valid_move? (board, index)
-    index.between? (0,8) && !position_taken? (board, index)
+    index.between? (0, 8) && !position_taken? (board, index)
 end
 
 def position_taken(board, index)
-  if board index == "" || board index == " " || board index == nil
+  if board[index] == "" || board[index] == " " || board[index] == nil
     return false
   else
     return true
